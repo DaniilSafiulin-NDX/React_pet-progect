e1f3f12b0feb7e92e24037fa02868c2955e22ee5
Daniil Safiulin <73942815+DaniilSafiulin-NDX@users.noreply.github.com>
2022-02-24 11:04:54 +0700

    Add files via upload
    My pet project

---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>
+                Ну как вам сказать... бюджета на эту страницу не хватило))
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
